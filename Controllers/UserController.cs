92298a320b7955b350b0d46f27bc4dd8111f8efb
TalhaBhutta <63284502+TalhaBhutta@users.noreply.github.com>
2022-06-29 23:50:06 +0500

    GetLatest

---
+++ Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCuisine.Models;
+
+namespace NetCuisine.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateSignUp(SignUpModel signUpModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/SignUpModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NetCuisine.Models
+{
+    public class SignUpModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public decimal Phone { get; set; }
+        public string Password { get; set; }
+    }
+}
